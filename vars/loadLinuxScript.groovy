780689d7f63a746f376251b1e56c87dd76a0756c
ganesh770924 <80471973+ganesh770924@users.noreply.github.com>
2022-02-03 17:54:48 +0530

    Update loadLinuxScript.groovy

--- vars/loadLinuxScript.groovy
+++ vars/loadLinuxScript.groovy
@@ -1,1 +1,5 @@
-
+def call(Map config=[:]){
+  def scriptcontents =libraryResource "src/hexaware/scripts/${config.name}" 
+  writeFile file: "${config.name}",text: scriptcontents
+  sh "chmod a+x ./${config.name}"
+}
